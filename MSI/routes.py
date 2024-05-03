9f08358779ed5b18cbd1133c80fcdb567d1ea923
EpicKOM <remi.fabre26@gmail.com>
2024-05-03 17:57:43 +0200

    Card Creation

--- MSI/routes.py
+++ MSI/routes.py
@@ -3,6 +3,11 @@ from flask import render_template
 
 
 @app.route("/")
-@app.route("/test/")
-def test():
-    return render_template("layout.html")
+@app.route("/meteo-live/saint-ismier/")
+def meteo_live_saint_ismier():
+    return render_template("meteo_live_saint_ismier.html")
+
+
+@app.route("/previsions/")
+def previsions():
+    return render_template("previsions.html")
