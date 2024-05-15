c65d09b8b1a3e36214c3b60226a01e4ae09eecc1
EpicKOM <remi.fabre26@gmail.com>
2024-05-15 12:11:46 +0200

    Start Chart

--- MSI/models/utils.py
+++ MSI/models/utils.py
@@ -8,7 +8,11 @@ class ModelUtils:
     @staticmethod
     def get_check_reception(cls):
         """
-        Si la réception des données météo remonte à plus de 3h alors affichage d'un message d'erreur
+        Checks if there has been a reception error based on the time elapsed
+        since the last record was received.
+
+        Returns:
+            bool: True if there is a reception error, False otherwise.
         """
         current_time = datetime.datetime.now()
         last_record_datetime = ModelUtils.get_last_record_datetime(cls)
@@ -25,16 +29,32 @@ class ModelUtils:
 
     @staticmethod
     def get_last_record(cls):
+        """
+        Retrieves the last record from the database table associated with the class.
+
+        Returns:
+            Model or None: The last record retrieved from the database table, or None if no records are found.
+        """
         return cls.query.order_by(cls.id.desc()).first()
 
     @staticmethod
     def get_wind_direction(wind_angle):
+        """
+        Determines the wind direction based on the given wind angle.
+
+        Args:
+            wind_angle (float): The wind angle in degrees.
+
+        Returns:
+            str: The wind direction as a cardinal or intercardinal direction abbreviation.
+        """
         COMPASS_ROSE = [0, 22.5, 45, 67.5, 90, 112.5, 135, 157.5, 180, 202.5, 225, 247.5, 270, 292.5, 315, 337.5, 360]
         compass_rose_angle = min(COMPASS_ROSE, key=lambda x: abs(x - wind_angle))
 
         directions = {0: "N", 22.5: "NNE", 45: "NE", 67.5: "ENE", 90: "E", 112.5: "ESE", 135: "SE", 157.5: "SSE",
                       180: "S",
                       202.5: "SSO", 225: "SO", 247.5: "OSO", 270: "O", 292.5: "ONO", 315: "NO", 337.5: "NNO", 360: "N"}
+
         return directions.get(compass_rose_angle)
 
     @staticmethod
@@ -85,7 +105,7 @@ class ModelUtils:
                                                                    cls.rain_1h.isnot(None)).scalar()
 
         rain_1h = {"rain_1h": round(data_rain_1h, 1) if data_rain_1h is not None else "-",
-                   "rain_1h_date": f"Mesure entre {rain_measurement_start_date.strftime('%H:%M')} et "
+                   "rain_1h_date": f"Mesure effectuée entre {rain_measurement_start_date.strftime('%H:%M')} et "
                                    f"{rain_measurement_end_date.strftime('%H:%M')}" if data_rain_1h is not None else "-"
                    }
 
