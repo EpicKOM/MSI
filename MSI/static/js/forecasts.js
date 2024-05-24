90e9d1aa5c235ddabb6a1731eb1234ff678fd608
EpicKOM <remi.fabre26@gmail.com>
2024-05-24 18:53:45 +0200

    forecasts js

--- MSI/static/js/forecasts.js
+++ MSI/static/js/forecasts.js
@@ -1,6 +1,29 @@
 $(document).ready(function() {
     $('.forecast-items').on('click', function() {
-        $('.forecast-items').css('background-color', '');
-        $(this).css('background-color', 'rgba(39, 39, 42, 1)');
+        $('.forecast-items').removeClass('active');
+        $(this).addClass('active');
+
+        let dayNumber = parseInt($(this).data('value'), 10);
     });
-});
+
+    $('#collapseButton').click(function() {
+        let tableIsCollapsed = ($("#forecasts-table").attr("data-value") === "true");
+
+        if (tableIsCollapsed) {
+            $("#forecasts-table").attr("data-value", "false");
+            let collapseTr = $("tr.hide");
+            collapseTr.each(function() {
+                $(this).removeClass('hide').addClass('show');
+            })
+        }
+
+        else {
+             $("#forecasts-table").attr("data-value", "true");
+            let collapseTr = $("tr.show");
+
+            collapseTr.each(function() {
+                $(this).removeClass('show').addClass('hide');
+            })
+        }
+    });
+})
