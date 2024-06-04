bb1bf97d0d3cefb3a31f154027e43cced4ed37ca
EpicKOM <remi.fabre26@gmail.com>
2024-06-04 15:29:44 +0200

    ajax forecasts update

--- MSI/api/forecasts.py
+++ MSI/api/forecasts.py
@@ -74,12 +74,37 @@ class ForecastsApi:
             if not all(key in forecasts_data for key in keys):
                 raise KeyError("Une des clés attendues est manquante dans les données du fichier JSON")
 
+            date = forecasts_data["time"][index]
+            snow_fraction = forecasts_data["snowfraction"][index]
+
             results = {
-                "date": forecasts_data["time"][index],
+                "date": ForecastsApi.get_date_french_format(date),
+                "day_name": ForecastsApi.get_french_day_name(date),
                 "pictocode": f"{forecasts_data['pictocode'][index]:02d}",
+                "predictability_label": ForecastsApi.get_predictability_label(forecasts_data["predictability_class"][index]),
+                "predictability": forecasts_data["predictability"][index],
                 "temperature_min": round(forecasts_data["temperature_min"][index]),
                 "temperature_mean": round(forecasts_data["temperature_mean"][index]),
                 "temperature_max": round(forecasts_data["temperature_max"][index]),
+                "felttemperature_min": round(forecasts_data["felttemperature_min"][index]),
+                "felttemperature_mean": round(forecasts_data["felttemperature_mean"][index]),
+                "felttemperature_max": round(forecasts_data["felttemperature_max"][index]),
+                "precipitation": round(forecasts_data["precipitation"][index]),
+                "precipitation_hours": forecasts_data["precipitation_hours"][index],
+                "precipitation_probability": forecasts_data["precipitation_probability"][index],
+                "convective_precipitation": round(forecasts_data["convective_precipitation"][index]),
+                "snow_fraction": ForecastsApi.get_precipitation_fraction(snow_fraction, "snow"),
+                "rain_fraction": ForecastsApi.get_precipitation_fraction(snow_fraction, "rain"),
+                "windspeed_min": round(forecasts_data["windspeed_min"][index] * 3.6),
+                "windspeed_mean": round(forecasts_data["windspeed_mean"][index] * 3.6),
+                "windspeed_max": round(forecasts_data["windspeed_max"][index] * 3.6),
+                "wind_direction": forecasts_data["winddirection"][index],
+                "sealevelpressure_min": forecasts_data["sealevelpressure_min"][index],
+                "sealevelpressure_mean": forecasts_data["sealevelpressure_mean"][index],
+                "sealevelpressure_max": forecasts_data["sealevelpressure_max"][index],
+                "relativehumidity_min": forecasts_data["relativehumidity_min"][index],
+                "relativehumidity_mean": forecasts_data["relativehumidity_mean"][index],
+                "relativehumidity_max": forecasts_data["relativehumidity_max"][index],
             }
             
             return results
@@ -121,3 +146,27 @@ class ForecastsApi:
         date_fr = date_obj.strftime("%d/%m/%Y")
 
         return date_fr
+
+    @staticmethod
+    def get_predictability_label(predictability_class):
+        predictability_label = {
+            1: "Très faible",
+            2: "Faible",
+            3: "Moyenne",
+            4: "Élevée",
+            5: "Très élevée",
+        }
+
+        return predictability_label[predictability_class]
+
+    @staticmethod
+    def get_precipitation_fraction(snow_fraction, precipitation_type):
+        snow = snow_fraction * 100
+        rain = 100 - snow
+
+        precipitation_fraction = {
+            "snow": snow,
+            "rain": rain,
+        }
+
+        return precipitation_fraction[precipitation_type]
--- MSI/static/js/forecasts.js
+++ MSI/static/js/forecasts.js
@@ -33,22 +33,81 @@ $(document).ready(function() {
 
 //----------------Ajax request------------------------------------------------------------------------------------------
 function ajaxRequest(dayNumber) {
+
     $.ajax({
         type : 'POST',
         url : '/data/forecasts',
         data : {'day_number': dayNumber},
 
         success:function(results) {
+
+            // Forecasts Header
             $('#pictocode').attr('src', `https://static.meteoblue.com/assets/images/picto/${results['forecasts_data']['pictocode']}_iday.svg`);
+            $('#dayName').text(results['forecasts_data']['day_name']);
+            $('#date').text(results['forecasts_data']['date']);
+
+            let predictabilityLabel = results['forecasts_data']['predictability_label'];
+
+            $('#predictabilityLabel').text(predictabilityLabel);
+            switch(predictabilityLabel) {
+                case "Très faible":
+                    break;
+
+                case "Faible":
+                    break;
+
+                case "Moyenne":
+                    $('#predictabilityLabel').removeClass().addClass('text-amber-color');
+                    $('#predictability').removeClass().addClass('progress-bar text-dark fw-bold bg-amber-color');
+                    break;
+
+                case "Élevée":
+                    $('#predictabilityLabel').removeClass().addClass('text-success');
+                    $('#predictability').removeClass().addClass('progress-bar text-dark fw-bold bg-success');
+                    break;
+
+                case "Très élevée":
+                    break;
+
+                default:
+            }
+            $('#predictability').css('width', `${results['forecasts_data']['predictability']}%`);
+            $('#predictability').text(`${results['forecasts_data']['predictability']}%`);
+
+            // Forecasts Table
             $('#temperature_min').text(results['forecasts_data']['temperature_min']);
-            $('#temperature_mean').text(results['forecasts_data']['temperature_mean']);;
-            $('#temperature_max').text(results['forecasts_data']['temperature_max']);;
+            $('#temperature_mean').text(results['forecasts_data']['temperature_mean']);
+            $('#temperature_max').text(results['forecasts_data']['temperature_max']);
+            $('#felttemperature_min').text(results['forecasts_data']['felttemperature_min']);
+            $('#felttemperature_mean').text(results['forecasts_data']['felttemperature_mean']);
+            $('#felttemperature_max').text(results['forecasts_data']['felttemperature_max']);
+
+            $('#precipitation').text(results['forecasts_data']['precipitation']);
+            $('#precipitation_hours').text(results['forecasts_data']['precipitation_hours']);
+            $('#precipitation_probability').css('width', `${results['forecasts_data']['precipitation_probability']}%`);
+            $('#precipitation_probability').text(`${results['forecasts_data']['precipitation_probability']}%`);
+            $('#convective_precipitation').text(results['forecasts_data']['convective_precipitation']);
+            $('#snow_fraction').text(results['forecasts_data']['snow_fraction']);
+            $('#rain_fraction').text(results['forecasts_data']['rain_fraction']);
+
+            $('#windspeed_min').text(results['forecasts_data']['windspeed_min']);
+            $('#windspeed_mean').text(results['forecasts_data']['windspeed_mean']);
+            $('#windspeed_max').text(results['forecasts_data']['windspeed_max']);
+            $('#windDirectionArrow').css('transform', `rotate(${results['forecasts_data']['wind_direction']}deg)`);
+
+            $('#sealevelpressure_min').text(results['forecasts_data']['sealevelpressure_min']);
+            $('#sealevelpressure_mean').text(results['forecasts_data']['sealevelpressure_mean']);
+            $('#sealevelpressure_max').text(results['forecasts_data']['sealevelpressure_max']);
+            $('#relativehumidity_min').text(results['forecasts_data']['relativehumidity_min']);
+            $('#relativehumidity_mean').text(results['forecasts_data']['relativehumidity_mean']);
+            $('#relativehumidity_max').text(results['forecasts_data']['relativehumidity_max']);
         },
 
         error:function() {
         },
 
         complete:function() {
+
         },
     })
 }
