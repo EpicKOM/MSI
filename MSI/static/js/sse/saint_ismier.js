3e928c2a12342057258debd29b9f91624bc205b2
EpicKOM <remi.fabre26@gmail.com>
2024-11-29 18:02:25 +0100

    JS live data

--- MSI/static/js/sse/saint_ismier.js
+++ MSI/static/js/sse/saint_ismier.js
@@ -8,11 +8,34 @@ $(document).ready(function() {
         let dailyExtremes = data.daily_extremes;
 
         console.log(currentWeatherData);
+        let update_datetime = currentWeatherData.update_datetime;
         let temperature = currentWeatherData.temperature.toFixed(1);
+        let temperature_trend = currentWeatherData.temperature_trend;
         let rain_1h = currentWeatherData.rain_1h.toFixed(1);
         let rain_24h = currentWeatherData.rain_24h.toFixed(1);
+        let wind_speed = currentWeatherData.wind_speed;
+        let gust_speed = currentWeatherData.gust_speed;
+        let wind_angle = currentWeatherData.wind_angle;
+        let wind_direction = currentWeatherData.wind_direction;
+        let humidity = currentWeatherData.humidity;
+        let pressure = currentWeatherData.pressure.toFixed(1);
 
-        $('#liveTemperature').text(`${temperature} °C`);
+        // Mise à jour section datetime
+        $("#liveDateTime").text(`Dernière mise à jour : ${update_datetime}`);
+        // Mise à jour section température
+        let iconTemperature = "";
+        if (temperature_trend === "down") {
+            iconTemperature =
+                '<i class="fa-solid fa-arrow-turn-down text-info ms-2 ms-xl-1 ms-xxl-2 opacity-75 fs-6"></i>';
+
+        }
+        else if (temperature_trend === "up") {
+            iconTemperature =
+                '<i class="fa-solid fa-arrow-turn-up text-danger ms-2 ms-xl-1 ms-xxl-2 opacity-75 fs-6"></i>';
+        }
+
+        // Mettre à jour le contenu de l'élément
+        $("#liveTemperature").html(`${temperature} °C ${iconTemperature}`);
     };
 
     // Gestion des erreurs
