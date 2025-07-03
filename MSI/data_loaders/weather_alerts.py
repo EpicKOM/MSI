900740cf5162ee131d8fc40dd6e1247840d766b9
EpicKOM <remi.fabre26@gmail.com>
2025-07-03 21:30:11 +0200

    vigilance json

--- MSI/data_loaders/weather_alerts.py
+++ MSI/data_loaders/weather_alerts.py
@@ -7,11 +7,6 @@ weather_alerts = load_json(json_path)
 
 def get_weather_alerts() -> dict:
 
-    return {
-        "max_color_id": weather_alerts["max_color_id"],
-        "max_alert_message": weather_alerts["max_alert_message"],
-        "results": [
-            {"phenomenon": item["phenomenon"], "color_id": item["color_id"], "alert_message": item["alert_message"]}
-            for item in weather_alerts["results"]
-        ]
-    }
+    return weather_alerts
+
+
