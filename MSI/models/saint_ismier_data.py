ff6e064df6a80fe092921f9457362805e5266671
EpicKOM <remi.fabre26@gmail.com>
2025-09-04 20:53:05 +0200

    typing saint_ismier_data

--- MSI/models/saint_ismier_data.py
+++ MSI/models/saint_ismier_data.py
@@ -1,5 +1,6 @@
 from MSI import app, db
 from MSI.models import MeteoLiveUtils
+from typing import Dict, Any, List
 
 
 class SaintIsmierData(db.Model):
@@ -15,7 +16,7 @@ class SaintIsmierData(db.Model):
     temperature_trend = db.Column(db.String(6))
 
     @classmethod
-    def get_data_status(cls):
+    def get_data_status(cls) -> Dict[str, bool]:
         try:
             is_table_empty = cls.query.first() is None
             data_status = {"is_table_empty": is_table_empty,
@@ -28,16 +29,26 @@ class SaintIsmierData(db.Model):
 
         except Exception:
             app.logger.exception(
-                "[SaintIsmierData::get_data_status] - Erreur lors de la récupération du status des données de la table "
-                "saint_ismier_data.")
+                "[SaintIsmierData::get_data_status] - Erreur lors de la récupération du status des données de la table"
+                " saint_ismier_data."
+            )
+
+            return {
+                "is_table_empty": True,
+                "is_data_fresh": False
+            }
 
     @classmethod
-    def get_current_weather_data(cls):
+    def get_current_weather_data(cls) -> Dict[str, Any]:
         try:
             last_record = MeteoLiveUtils.get_last_record(cls)
 
             base_weather_data = {"update_datetime": last_record.date_time.strftime("%d/%m/%Y à %H:%M"),
-                                 "temperature": round(last_record.temperature, 1) if last_record.temperature is not None else None,
+                                 "temperature": (
+                                     round(last_record.temperature, 1)
+                                     if last_record.temperature is not None
+                                     else None
+                                 ),
                                  "humidity": last_record.humidity if last_record.humidity is not None else None,
                                  "wind_speed": last_record.wind_speed if last_record.wind_speed is not None else None,
                                  "gust_speed": last_record.gust_speed if last_record.gust_speed is not None else None,
@@ -45,8 +56,16 @@ class SaintIsmierData(db.Model):
                                  "wind_direction": MeteoLiveUtils.get_wind_direction(
                                      last_record.wind_angle) if last_record.wind_angle is not None else None,
                                  "rain_24h": MeteoLiveUtils.get_rain_24h(cls),
-                                 "pressure": round(last_record.pressure, 1) if last_record.pressure is not None else None,
-                                 "temperature_trend": last_record.temperature_trend if last_record.temperature_trend else "stable"
+                                 "pressure": (
+                                     round(last_record.pressure, 1)
+                                     if last_record.pressure is not None
+                                     else None
+                                 ),
+                                 "temperature_trend": (
+                                     last_record.temperature_trend
+                                     if last_record.temperature_trend else
+                                     "stable"
+                                 )
                                  }
 
             rain_data = MeteoLiveUtils.get_rain_1h(cls)
@@ -56,10 +75,26 @@ class SaintIsmierData(db.Model):
 
         except Exception:
             app.logger.exception(
-                "[SaintIsmierData::get_current_weather_data] - Erreur lors de la récupération des données actuelles.")
+                "[SaintIsmierData::get_current_weather_data] - Erreur lors de la récupération des données actuelles."
+            )
+
+            return {
+                "update_datetime": None,
+                "temperature": None,
+                "humidity": None,
+                "wind_speed": None,
+                "gust_speed": None,
+                "wind_angle": None,
+                "wind_direction": None,
+                "rain_24h": None,
+                "rain_1h": None,
+                "rain_1h_date": None,
+                "pressure": None,
+                "temperature_trend": "stable"
+            }
 
     @classmethod
-    def get_daily_extremes(cls):
+    def get_daily_extremes(cls) -> Dict[str, Any]:
         try:
             daily_temperature_extremes = MeteoLiveUtils.get_daily_temperature_extremes(cls)
             daily_max_gust = MeteoLiveUtils.get_daily_max_gust(cls)
@@ -70,11 +105,20 @@ class SaintIsmierData(db.Model):
 
         except Exception:
             app.logger.exception(
-                "[SaintIsmierData::get_daily_extremes] - Erreur lors de la récupération des températures et des rafales "
-                "extrêmes du jour.")
+                "[SaintIsmierData::get_daily_extremes] - Erreur lors de la récupération des températures et des rafales"
+                " extrêmes du jour."
+            )
+
+            return {
+                "tmax": None,
+                "tmin": None,
+                "tmax_time": None,
+                "tmin_time": None,
+                "gust_max": None,
+                "gust_max_time": None}
 
     @classmethod
-    def current_charts_data(cls, data_name, interval_duration):
+    def current_charts_data(cls, data_name: str, interval_duration: int) -> Dict[str, List[Any]]:
         try:
             column_mapping = {
                 "temperature": [cls.date_time, cls.temperature],
